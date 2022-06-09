829c2d0a211e368e0cac2cdeb31a9a2dcfcfcc11
Hoto Cocoa <cocoa@hoto.us>
2022-06-09 12:14:26 +0900

    First Commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo ln -fs /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cat .zshrc > $HOME/.zshrc
+cat .p10k.zsh > $HOME/.p10k.zsh
+
